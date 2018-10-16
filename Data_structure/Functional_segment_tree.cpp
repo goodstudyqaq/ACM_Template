50a3e29f71204ffc57f761cffb29a3fd26776a6a
Algorithm <goodstudyQAQ@163.com>
2018-10-16 11:13:01 +0800

    Update Functional_segment_tree.cpp

--- Data_structure/Functional_segment_tree.cpp
+++ Data_structure/Functional_segment_tree.cpp
@@ -1,106 +1,411 @@
-//查询区间第K大，而且没有修改。
-#include<bits/stdc++.h>
+//静态区间第k大
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<stdio.h>
 using namespace std;
-const int MAXN = 100010;
-const int M = MAXN * 30;
-int n,q,m,tot;
-int a[MAXN], t[MAXN];
-int T[M], lson[M], rson[M], c[M];
-void Init_hash()
-{
-    for(int i = 1; i <= n;i++)
-        t[i] = a[i];
-    sort(t+1,t+1+n);
-    m = unique(t+1,t+1+n)-t-1;
-}
+const int maxn=1e5+5;
+int a[maxn],x[maxn],xnum;
+int rt[maxn];
+int c[maxn*30],lson[maxn*30],rson[maxn*30],tot;
+
 int build(int l,int r)
 {
-    int root = tot++;
-    c[root] = 0;
-    if(l != r)
+    int root=tot++;
+    c[root]=0;
+    if(l!=r)
     {
-        int mid = (l+r)>>1;
-        lson[root] = build(l,mid);
-        rson[root] = build(mid+1,r);
+        int m=l+r>>1;
+        lson[root]=build(l,m);
+        rson[root]=build(m+1,r);
     }
     return root;
 }
-int hash(int x)
+//递归版
+int update(int pre,int L,int C,int l,int r)
 {
-    return lower_bound(t+1,t+1+m,x) - t;
-}
-int update(int root,int pos,int val)
-{
-    int newroot = tot++, tmp = newroot;
-    c[newroot] = c[root] + val;
-    int l = 1, r = m;
-    while(l < r)
+    int root=tot++;
+    lson[root]=lson[pre];
+    rson[root]=rson[pre];
+    c[root]=c[pre]+C;
+    if(l!=r)
     {
-        int mid = (l+r)>>1;
-        if(pos <= mid)
+        int mid=l+r>>1;
+        if(L<=mid)
+            lson[root]=update(lson[pre],L,C,l,mid);
+        else rson[root]=update(rson[pre],L,C,mid+1,r);
+    }
+    return root;
+}
+int query(int u,int v,int l,int r,int k)
+{
+    if(l==r)return l;
+    int mid=l+r>>1;
+    int x=c[lson[v]]-c[lson[u]];
+    if(x>=k)return query(lson[u],lson[v],l,mid,k);
+    k-=x;
+    return query(rson[u],rson[v],mid+1,r,k);
+
+}
+//非递归版
+int update(int pre,int L,int C)
+{
+    int root=tot++,tmp=root;
+    c[root]=c[pre]+C;
+    int l=1,r=xnum;
+    while(l<r)
+    {
+        int mid=l+r>>1;
+        if(L<=mid)
         {
-            lson[newroot] = tot++; rson[newroot] = rson[root];
-            newroot = lson[newroot]; root = lson[root];
-            r = mid;
+            lson[root]=tot++;rson[root]=rson[pre];
+            root=lson[root];pre=lson[pre];
+            r=mid;
         }
         else
         {
-            rson[newroot] = tot++; lson[newroot] = lson[root];
-            newroot = rson[newroot]; root = rson[root];
-            l = mid+1;
+            rson[root]=tot++;lson[root]=lson[pre];
+            root=rson[root];pre=rson[pre];
+            l=mid+1;
         }
-        c[newroot] = c[root] + val;
+        c[root]=c[pre]+C;
     }
     return tmp;
 }
-int query(int left_root,int right_root,int k)
+int query(int u,int v,int k)
 {
-    int l = 1, r = m;
-    while( l < r)
+    int l=1,r=xnum;
+    while(l<r)
     {
-        int mid = (l+r)>>1;
-        if(c[lson[left_root]]-c[lson[right_root]] >= k )
+        int mid=l+r>>1;
+        if(c[lson[v]]-c[lson[u]]>=k)
         {
-            r = mid;
-            left_root = lson[left_root];
-            right_root = lson[right_root];
+            r=mid;
+            v=lson[v];
+            u=lson[u];
         }
         else
         {
-            l = mid + 1;
-            k -= c[lson[left_root]] - c[lson[right_root]];
-            left_root = rson[left_root];
-            right_root = rson[right_root];
+            l=mid+1;
+            k-=c[lson[v]]-c[lson[u]];
+            v=rson[v];
+            u=rson[u];
         }
     }
     return l;
 }
 int main()
 {
-    //freopen("in.txt","r",stdin);
-    //freopen("out.txt","w",stdout);
-    while(scanf("%d%d",&n,&q) == 2)
+    int n,m;
+    while(~scanf("%d %d",&n,&m))
     {
-        tot = 0;
-        for(int i = 1;i <= n;i++)
-            scanf("%d",&a[i]);
-        Init_hash();
-        T[n+1] = build(1,m);
-        for(int i = n;i ;i--)
+        tot=0;
+        for(int i=1;i<=n;i++)scanf("%d",&a[i]),x[i]=a[i];
+        sort(x+1,x+1+n);
+        xnum=unique(x+1,x+1+n)-x-1;
+        rt[0]=build(1,xnum);
+        for(int i=1;i<=n;i++)
         {
-            int pos = hash(a[i]);
-            T[i] = update(T[i+1],pos,1);
+            int idx=lower_bound(x+1,x+1+xnum,a[i])-x;
+            rt[i]=update(rt[i-1],idx,1);
         }
-        while(q--)
+        while(m--)
         {
             int l,r,k;
-            scanf("%d%d%d",&l,&r,&k);
-            printf("%d\n",t[query(T[l],T[r+1],k)]);
+            scanf("%d %d %d",&l,&r,&k);
+            printf("%d\n",x[query(rt[l-1],rt[r],k)]);
         }
     }
     return 0;
 }
-//*******************************************************
+/*******************************************************/
+//动态区间第k大
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn=60005;
+const int N=2500010;
+int a[maxn],x[maxn],xnum;
+int lson[N],rson[N],c[N];
+int T[maxn],tot,S[maxn];
+int build(int l,int r)
+{
+    int root=tot++;
+    c[root]=0;
+    if(l<r)
+    {
+        int m=l+r>>1;
+        lson[root]=build(l,m);
+        rson[root]=build(m+1,r);
+    }
+    return root;
+}
+
+int update(int pre,int L,int C)
+{
+    int newroot=tot++,tmp=newroot;
+    int l=1,r=xnum;
+    c[newroot]=c[pre]+C;
+    while(l<r)
+    {
+        int m=l+r>>1;
+        if(L<=m)
+        {
+            lson[newroot]=tot++;rson[newroot]=rson[pre];
+            newroot=lson[newroot];pre=lson[pre];
+            r=m;
+        }
+        else
+        {
+            rson[newroot]=tot++;lson[newroot]=lson[pre];
+            newroot=rson[newroot];pre=rson[pre];
+            l=m+1;
+        }
+        c[newroot]=c[pre]+C;
+    }
+    return tmp;
+}
+
+//
+
+struct Query
+{
+    int kind;
+    int l,r,k;
+}qs[10005];
+int n;
+#define lowbit(x) x&-x
+void add(int i,int x,int val)
+{
+    while(i<=n)
+    {
+        S[i]=update(S[i],x,val);
+        i+=lowbit(i);
+    }
+}
+int used[maxn];
+int query(int x)
+{
+    int res=0;
+    while(x)
+    {
+        res+=c[lson[used[x]]];
+        x-=lowbit(x);
+    }
+    return res;
+}
+int query(int u,int v,int k)
+{
+    int left_root=T[u];
+    int right_root=T[v];
+    for(int i=u;i;i-=lowbit(i))
+        used[i]=S[i];
+    for(int i=v;i;i-=lowbit(i))
+        used[i]=S[i];
+    int l=1,r=xnum;
+    while(l<r)
+    {
+        int m=l+r>>1;
+        int tmp=c[lson[right_root]]-c[lson[left_root]]+query(v)-query(u);
+        if(tmp>=k)
+        {
+            for(int i=u;i;i-=lowbit(i))used[i]=lson[used[i]];
+            for(int i=v;i;i-=lowbit(i))used[i]=lson[used[i]];
+            left_root=lson[left_root];right_root=lson[right_root];
+            r=m;
+        }
+        else
+        {
+            k-=tmp;
+            for(int i=u;i;i-=lowbit(i))used[i]=rson[used[i]];
+            for(int i=v;i;i-=lowbit(i))used[i]=rson[used[i]];
+            left_root=rson[left_root];right_root=rson[right_root];
+            l=m+1;
+        }
+    }
+    return l;
+}
+
+int main()
+{
+    int Tcas;
+    scanf("%d",&Tcas);
+    while(Tcas--)
+    {
+        tot=0;
+        int m;
+        scanf("%d %d",&n,&m);
+        for(int i=1;i<=n;i++)scanf("%d",&a[i]),x[i]=a[i];
+        xnum=n;
+        /*sort(x+1,x+1+n);
+        xnum=unique(x+1,x+1+n)-x-1;
+        T[0]=build(1,xnum);
+        for(int i=1;i<=n;i++)
+        {
+            int idx=lower_bound(x+1,x+1+n,a[i])-x;
+            T[i]=update(T[i-1],idx,1);
+        }*/
+        char s[3];
+        int l,r,k;
+        for(int i=1;i<=m;i++)
+        {
+            scanf("%s",s);
+            if(s[0]=='Q')
+            {
+                qs[i].kind=0;
+                scanf("%d %d %d",&l,&r,&k);
+                qs[i].l=l,qs[i].r=r,qs[i].k=k;
+            }
+            else
+            {
+                qs[i].kind=1;
+                scanf("%d %d",&l,&r);
+                qs[i].l=l,qs[i].r=r;
+                x[++xnum]=r;
+            }
+        }
+        sort(x+1,x+1+xnum);
+        xnum=unique(x+1,x+1+xnum)-x-1;
+        T[0]=build(1,xnum);
+        for(int i=1;i<=n;i++)
+        {
+            int idx=lower_bound(x+1,x+1+xnum,a[i])-x;
+            T[i]=update(T[i-1],idx,1);
+            S[i]=T[0];
+        }
+        for(int i=1;i<=m;i++)
+        {
+            if(qs[i].kind==0)
+            {
+                printf("%d\n",x[query(qs[i].l-1,qs[i].r,qs[i].k)]);
+            }
+            else
+            {
+                int idx=lower_bound(x+1,x+1+xnum,a[qs[i].l])-x;
+                add(qs[i].l,idx,-1);
+                idx=lower_bound(x+1,x+1+xnum,qs[i].r)-x;
+                add(qs[i].l,idx,1);
+                a[qs[i].l]=qs[i].r;
+            }
+        }
+    }
+    return 0;
+}
+/*******************************************************/
+//一般写法
+//hdu4348
+//一个长度为n的数组，4种操作 C l r d区间[l,r]都加1，当前时间戳加1 Q l r 查询当前时间戳[l,r]的所有数的和 H l r t 查询时间戳t区间[l,r]的和 B t将当前时间戳置为t.
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn=1e5+5;
+long long a[maxn];
+int lson[maxn*20],rson[maxn*20];
+long long sum[maxn*20];
+long long add[maxn*20];
+int T[maxn],tot;
+
+int build(int l,int r)
+{
+    int root=tot++;
+    add[root]=0;
+    if(l==r)
+    {
+        sum[root]=a[l];
+        return root;
+    }
+    int m=l+r>>1;
+    lson[root]=build(l,m);
+    rson[root]=build(m+1,r);
+    sum[root]=sum[lson[root]]+sum[rson[root]];
+    return root;
+}
+
+void pushup(int rt,int l,int r)
+{
+    int ls=lson[rt],rs=rson[rt];
+    int len=r-l+1;
+    sum[rt]=sum[ls]+sum[rs]+add[ls]*(len-len/2)+add[rs]*(len/2);
+}
+
+int update(int root,int L,int R,int C,int l,int r)
+{
+    int newroot=tot++;
+    add[newroot]=add[root];
+    if(L<=l&&r<=R)
+    {
+        lson[newroot]=lson[root];
+        rson[newroot]=rson[root];
+        add[newroot]=add[root]+C;
+        sum[newroot]=sum[root];
+        return newroot;
+    }
+    int m=l+r>>1;
+    if(L<=m)
+        lson[newroot]=update(lson[root],L,R,C,l,m);
+    else lson[newroot]=lson[root];
+    if(R>m)
+        rson[newroot]=update(rson[root],L,R,C,m+1,r);
+    else rson[newroot]=rson[root];
+    pushup(newroot,l,r);
+    return newroot;
+}
+
+long long query(int root,int L,int R,int l,int r,long long a)
+{
+
+    if(L<=l&&r<=R)
+    {
+        return sum[root]+(add[root]+a)*(r-l+1);
+    }
+    int m=l+r>>1;
+    long long ans=0;
+    if(L<=m)ans+=query(lson[root],L,R,l,m,a+add[root]);
+    if(R>m)ans+=query(rson[root],L,R,m+1,r,a+add[root]);
+    return ans;
+}
+
+int main()
+{
+    int n,m;
+    while(~scanf("%d %d",&n,&m))
+    {
+        tot=0;
+        for(int i=1;i<=n;i++)
+            scanf("%lld",&a[i]);
+        T[0]=build(1,n);
+        int t=0;
+        char s[3];
+        while(m--)
+        {
+            scanf("%s",s);
+            if(s[0]=='C')
+            {
+                int l,r,d;
+                scanf("%d %d %d",&l,&r,&d);
+                T[++t]=update(T[t-1],l,r,d,1,n);
+            }
+            else if(s[0]=='Q')
+            {
+                int l,r;
+                scanf("%d %d",&l,&r);
+                printf("%lld\n",query(T[t],l,r,1,n,0));
+            }
+            else if(s[0]=='H')
+            {
+                int l,r,_t;
+                scanf("%d %d %d",&l,&r,&_t);
+                printf("%lld\n",query(T[_t],l,r,1,n,0));
+            }
+            else
+            {
+                scanf("%d",&t);
+                tot=T[t+1];
+            }
+        }
+    }
+    return 0;
+}
+
+/******************************************************/
 //区间不同数
 #include<bits/stdc++.h>
 using namespace std;
@@ -203,181 +508,4 @@ int main()
     }
     return 0;
 }
-//**************************************************************
-//动态第k大
-#include<bits/stdc++.h>
-using namespace std;
-const int MAXN = 60010;
-const int M = 2500010;
-int n,q,m,tot;
-int a[MAXN], t[MAXN];
-int T[MAXN], lson[M], rson[M],c[M];
-int S[MAXN];
-struct Query
-{
-    int kind;
-    int l,r,k;
-}query[10010];
-void Init_hash(int k)
-{
-    sort(t,t+k);
-    m = unique(t,t+k) - t;
-}
-int hash(int x)
-{
-    return lower_bound(t,t+m,x)-t;
-}
-int build(int l,int r)
-{
-    int root = tot++;
-    c[root] = 0;
-    if(l != r)
-    {
-        int mid = (l+r)/2;
-        lson[root] = build(l,mid);
-        rson[root] = build(mid+1,r);
-    }
-    return root;
-}
-int Insert(int root,int pos,int val)
-{
-    int newroot = tot++, tmp = newroot;
-    int l = 0, r = m-1;
-    c[newroot] = c[root] + val;
-    while(l < r)
-    {
-        int mid = (l+r)>>1;
-        if(pos <= mid)
-        {
-            lson[newroot] = tot++; rson[newroot] = rson[root];
-            newroot = lson[newroot]; root = lson[root];
-            r = mid;
-        }
-        else
-        {
-            rson[newroot] = tot++; lson[newroot] = lson[root];
-            newroot = rson[newroot]; root = rson[root];
-            l = mid+1;
-        }
-        c[newroot] = c[root] + val;
-    }
-    return tmp;
-}
-int lowbit(int x)
-{
-    return x&(-x);
-}
-int use[MAXN];
-void add(int x,int pos,int val)
-{
-    while(x <= n)
-    {
-        S[x] = Insert(S[x],pos,val);
-        x += lowbit(x);
-    }
-}
-int sum(int x)
-{
-    int ret = 0;
-    while(x > 0)
-    {
-        ret += c[lson[use[x]]];
-        x -= lowbit(x);
-    }
-    return ret;
-}
-int Query(int left,int right,int k)
-{
-    int left_root = T[left-1];
-    int right_root = T[right];
-    int l = 0, r = m-1;
-    for(int i = left-1;i;i -= lowbit(i)) use[i] = S[i];
-    for(int i = right;i ;i -= lowbit(i)) use[i] = S[i];
-    while(l < r)
-    {
-        int mid = (l+r)/2;
-        int tmp = sum(right) - sum(left-1) + c[lson[right_root]] - c[lson[left_root]];
-        if(tmp >= k)
-        {
-            r = mid;
-            for(int i = left-1; i ;i -= lowbit(i))
-                use[i] = lson[use[i]];
-            for(int i = right; i; i -= lowbit(i))
-                use[i] = lson[use[i]];
-            left_root = lson[left_root];
-            right_root = lson[right_root];
-        }
-        else
-        {
-            l = mid+1;
-            k -= tmp;
-            for(int i = left-1; i;i -= lowbit(i))
-                use[i] = rson[use[i]];
-            for(int i = right;i ;i -= lowbit(i))
-                use[i] = rson[use[i]];
-            left_root = rson[left_root];
-            right_root = rson[right_root];
-        }
-    }
-    return l;
-}
-void Modify(int x,int p,int d)
-{
-    while(x <= n)
-    {
-        S[x] = Insert(S[x],p,d);
-        x += lowbit(x);
-    }
-}
-int main()
-{
-    //freopen("in.txt","r",stdin);
-    //freopen("out.txt","w",stdout);
-    int Tcase;
-    scanf("%d",&Tcase);
-    while(Tcase--)
-    {
-        scanf("%d%d",&n,&q);
-        tot = 0;
-        m = 0;
-        for(int i = 1;i <= n;i++)
-        {
-            scanf("%d",&a[i]);
-            t[m++] = a[i];
-        }
-        char op[10];
-        for(int i = 0;i < q;i++)
-        {
-            scanf("%s",op);
-            if(op[0] == 'Q')
-            {
-                query[i].kind = 0;
-                scanf("%d%d%d",&query[i].l,&query[i].r,&query[i].k);
-            }
-            else
-            {
-                query[i].kind = 1;
-                scanf("%d%d",&query[i].l,&query[i].r);
-                t[m++] = query[i].r;
-            }
-        }
-        Init_hash(m);
-        T[0] = build(0,m-1);
-        for(int i = 1;i <= n;i++)
-            T[i] = Insert(T[i-1],hash(a[i]),1);
-        for(int i = 1;i <= n;i++)
-            S[i] = T[0];
-        for(int i = 0;i < q;i++)
-        {
-            if(query[i].kind == 0)
-                printf("%d\n",t[Query(query[i].l,query[i].r,query[i].k)]);
-            else
-            {
-                Modify(query[i].l,hash(a[query[i].l]),-1);
-                Modify(query[i].l,hash(query[i].r),1);
-                a[query[i].l] = query[i].r;
-            }
-        }
-    }
-    return 0;
-}
+
